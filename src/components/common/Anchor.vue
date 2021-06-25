56e5b81fe9cb287fc527e6880790ed265af84e9c
yikl1004 <yikl1004@gmail.com>
2021-06-25 09:28:57 +0900

    [fix] <Link/>컴포넌트 name 수정(Anchor)

--- src/components/common/Anchor.vue
+++ src/components/common/Anchor.vue
@@ -11,7 +11,7 @@
 import { Component, Prop, Vue } from 'vue-property-decorator'
 
 @Component
-export default class SellyLink extends Vue {
+export default class Anchor extends Vue {
     @Prop({ type: String, default: '/', required: true })
     public href!: string
 
