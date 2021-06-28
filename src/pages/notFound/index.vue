f2b92d8f0b831b9740d62fa64fa23ea4e022a2b9
yikl1004 <yikl1004@gmail.com>
2021-06-28 10:53:38 +0900

    [add] 404페이지 추가(디자인 없음)

---
+++ src/pages/notFound/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container">
+        <h1>페이지를 찾을 수 없습니다.</h1>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+/**
+ * TODO: 404 페이지, 아직 디자인이 없음
+ */
+@Component
+export default class NotFoundPage extends Vue {}
+</script>
+
+<style scoped></style>
