6fdf246ff690612f7db78be6c4a31606c3dfe682
narie88 <leeclamp@hanmail.com>
2021-07-09 10:12:11 +0900

    로그인 기획서 수정 업뎃

--- src/pages/auth/SelectStore.vue
+++ src/pages/auth/SelectStore.vue
@@ -1,45 +1,59 @@
 <template>
-    <div class="container">
-        <div class="select-store-wrap">
-            <div class="select-store">
-                <div class="store-user-info">
-                    <strong>
-                        {{ workplaceOwnerName }} 사장님<br />
-                        사업자정보를 확인하세요.
-                    </strong>
-                    <p>
-                        현재 등록된 {{ workplaceList.length }}개의 사업자 가입이 가능합니다.<br />선택한 사업자만 셀리 서비스에 가입됩니다.
-                    </p>
+    <div>
+        <header class="page-header">
+            <h1>사업자 정보</h1>
+        </header>
+        <div class="container no-padding">
+            <div class="select-store-wrap">
+                <div class="select-store">
+                    <div class="store-user-info">
+                        <strong>
+                            {{ workplaceOwnerName }} 사장님<br />
+                            사업자정보를 확인하세요.
+                        </strong>
+
+                        <p>
+                            가입을 원하지 않는 사업자의 경우 <i class="mark"><span class="ir">체크</span></i
+                            >를 해지해주세요.<br />
+                            (정 사업자의 경우는 <i class="mark"><span class="ir">체크</span></i
+                            >해지가 불가합니다.)
+                        </p>
+                    </div>
+                    <div class="user-store-list">
+                        <CheckBoxBlock
+                            v-for="(item, index) in workplaceList"
+                            :id="`userStoreList${index}`"
+                            :key="`user-store-list-item-${index}`"
+                            :label="convertBizNoFormatter({ bizNo: item.bzno })"
+                            :biz-name="item.bzmanNm"
+                            :index="index"
+                            :checked="item.ltRgyn === 'Y'"
+                            :disabled="item.locaMcYn === 'Y'"
+                            name="group"
+                            @change="onSelectBizNo"
+                        />
+                    </div>
                 </div>
-                <div class="user-store-list">
-                    <CheckBoxBlock
-                        v-for="(item, index) in workplaceList"
-                        :id="`userStoreList${index}`"
-                        :key="`user-store-list-item-${index}`"
-                        :label="convertBizNoFormatter({ bizNo: item.bzno })"
-                        :biz-name="item.bzmanNm"
-                        :index="index"
-                        :checked="item.ltRgyn === 'Y'"
-                        :disabled="item.locaMcYn === 'Y'"
-                        name="group"
-                        @change="onSelectBizNo"
+
+                <div class="recommender-box">
+                    <LabelTitle label="추천인 코드(선택)" />
+                    <CheckBox2 label="추천인이 있으시면 체크해주세요." />
+                    <ButtonField
+                        id="recommenderCode"
+                        label="추천인 코드(선택)"
+                        :maxlength="9999"
+                        placeholder="추천인 코드 입력"
+                        :readonly="false"
+                        :disabled="false"
+                        button-text="확인"
+                        name="cert"
+                        @search="onClickRecommenderCode"
                     />
                 </div>
-                <ButtonField
-                    id="recommenderCode"
-                    label="추천인 코드(선택)"
-                    :maxlength="9999"
-                    placeholder="추천인 코드 입력"
-                    :readonly="false"
-                    :disabled="false"
-                    button-text="확인"
-                    name="cert"
-                    @search="onClickRecommenderCode"
-                />
             </div>
             <portal to="floating">
                 <BasicButton :disabled="!selectedWorkplace.length" size="large" @click="onNext">
-                    다음
+                    메인으로
                 </BasicButton>
             </portal>
         </div>
