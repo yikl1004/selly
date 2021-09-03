234eda58ceb9b2bd9510b582e9d044cdbcaf060b
narie88 <leeclamp@hanmail.com>
2021-09-03 16:22:18 +0900

    마케팅_미리보기팝업

--- src/components/marketing/PopupCouponPreview.vue
+++ src/components/marketing/PopupCouponPreview.vue
@@ -1,53 +1,79 @@
 <template>
-    <div class="popup-inner no-padding">
-        <div class="popup-coupon-preview">
-            <div class="touch-coupon-box">
-                <div class="touch-coupon-header">
-                    <h3>
+    <div class="popup-inner">
+        <div class="title-coupon-preview">
+            <strong>푸시메시지 홍보</strong>
+            <p>
+                롯데카드 앱 이용 고객에게 푸시메시지가 발송되며, 발송된 메시지는
+                앱 내 ‘알림’에서 확인 할 수 있습니다.
+            </p>
+        </div>
+        <div class="touch-coupon-box">
+            <div class="touch-coupon-header">
+                <div class="touch-coupon-right">
+                    <i class="ico">
                         <img
-                            src="/assets/marketing/logo-touch-coupon.png"
-                            alt="touch 로고"
+                            src="/assets/marketing/ico-coupon-alarm.png"
+                            alt=""
                         />
-                    </h3>
-                    <div class="touch-coupon-alarm">
-                        <i class="ico">
-                            <img
-                                src="/assets/marketing/ico-coupon-alarm.png"
-                                alt=""
-                            />
-                        </i>
-                        <i class="ico">
-                            <img
-                                src="/assets/marketing/ico-coupon-menu.png"
-                                alt=""
-                            />
-                        </i>
-                    </div>
+                    </i>
+                    <i class="ico">
+                        <img
+                            src="/assets/marketing/ico-coupon-menu.png"
+                            alt=""
+                        />
+                    </i>
                 </div>
-                <div class="touch-coupon-cont">
+            </div>
+            <div class="touch-coupon-cont">
+                <strong class="touch-tit">로카알리미</strong>
+                <div class="touch-cont-box">
                     <span class="date">2021. 07. 01(월)</span>
-                    <p>
-                        <em>[광고]</em>
-                        [김로카 고객님의 주변의
-                        <strong>이층집 강남점</strong>에서
-                        <strong>7월 31일</strong>까지 결제금액의
-                        <strong>10% 할인행사</strong>를 진행중이니 혜택을
-                        받아보세요.
-                    </p>
+                    <div class="touch-message-box">
+                        <p class="subject">
+                            (광고) <strong>[첫 결제 10%할인]</strong>
+                        </p>
+                        <p>
+                            김로카님 주변1km에 있는 가맹점명에서 결제일 할인을
+                            받아보세요.
+                        </p>
+                    </div>
                     <div class="img">
                         <!--샘플이미지-->
                         <img
-                            src="/assets/marketing/logo-touch-coupon.png"
+                            src="/assets/marketing/@img-coupon-preview.png"
                             alt=""
                         />
                     </div>
-
-                    <BulletList
-                        :list="[{ text: '수신거부 : 설정-알림설정-혜택' }]"
-                    />
                 </div>
             </div>
-            <div class="coupon-sample-list">
+        </div>
+
+        <div class="title-coupon-preview">
+            <strong>롯데카드 앱 쿠폰 노출</strong>
+            <p>
+                ‘롯데카드 앱 > 혜택 > TOUCH’에서 신청한 쿠폰을 확인할 수
+                있습니다.
+            </p>
+        </div>
+        <div class="coupon-sample-list">
+            <div class="touch-coupon-header">
+                <i class="ico">
+                    <img
+                        src="/assets/marketing/ico-preview-arrow2.png"
+                        alt=""
+                    />
+                </i>
+                <h3>TOUCH</h3>
+                <div class="touch-coupon-right">
+                    <i class="ico">
+                        <img
+                            src="/assets/marketing/ico-preview-menu.png"
+                            alt=""
+                        />
+                    </i>
+                </div>
+            </div>
+            <div class="sample-item-cont">
                 <div class="coupon-sample-item">
                     <div class="coupon-sample-inner">
                         <div class="coupon-info">
@@ -58,12 +84,13 @@
                             <span class="date">21.06.01~21.06.30</span>
                             <div class="img-ci">
                                 <img
-                                    src="/assets/marketing/ico-coupon-ci.svg"
+                                    src="/assets/marketing/@ico-ci.png"
                                     alt=""
                                 />
                             </div>
                         </div>
                         <div class="use-box">
+                            <span class="area">#지역명</span>
                             <span class="ico">사용가능</span>
                         </div>
                     </div>
@@ -77,12 +104,13 @@
                             <span class="date">21.06.01~21.06.30</span>
                             <div class="img-ci">
                                 <img
-                                    src="/assets/marketing/ico-coupon-ci.svg"
+                                    src="/assets/marketing/@ico-ci.png"
                                     alt=""
                                 />
                             </div>
                         </div>
                         <div class="use-box">
+                            <span class="area">#지역명</span>
                             <span class="ico">사용가능</span>
                         </div>
                     </div>
@@ -97,12 +125,13 @@
                             <span class="date">21.06.01~21.06.30</span>
                             <div class="img-ci">
                                 <img
-                                    src="/assets/marketing/ico-coupon-ci.svg"
+                                    src="/assets/marketing/@ico-ci.png"
                                     alt=""
                                 />
                             </div>
                         </div>
                         <div class="use-box">
+                            <span class="area">#지역명</span>
                             <span class="ico">사용가능</span>
                         </div>
                     </div>
